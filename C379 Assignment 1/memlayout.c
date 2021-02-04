33d00dfef9a24f24f1f499fe09dfa798ff7af1ab
Allan Manuba <allanmanuba@gmail.com>
2021-02-03 19:02:58 -0700

    Document memlayout

--- C379 Assignment 1/memlayout.c
+++ C379 Assignment 1/memlayout.c
@@ -1,56 +1,71 @@
-/* todo: implement */
-
 #include "memlayout.h"
 
+/**
+ * If USER_PAGE_SIZE is defined, PAGE_SIZE is equal to USER_PAGE_SIZE.
+ * Otherwise, PAGE_SIZE is 4096.
+ */
 #ifdef USER_PAGE_SIZE
 const unsigned int PAGE_SIZE = USER_PAGE_SIZE;
 #else
 const unsigned int PAGE_SIZE = 4096;
 #endif
 
-sigjmp_buf env;
-sigjmp_buf ro_env;
-sigjmp_buf no_env;
-
-struct memregion *new_memregion(void *from, unsigned char mode)
+/**
+ * @brief Used by get_mem_layout to test to what extent can we access a page
+ * 
+ * When a signal is handled, we simply return to where we last called sigsetjmp,
+ * and ignore the exception.
+ * 
+ * @param sig The signal number which handle_signal handled
+ */
+void handle_signal(int sig)
 {
-    struct memregion *m = malloc(sizeof *m);
-    m->from = from;
-    m->to = NULL;
-    m->mode = mode;
-    return m;
+    if (sig == SIGSEGV) {
+       (void)signal(SIGSEGV, SIG_IGN);
+    } else if (sig == SIGBUS) {
+        (void)signal(SIGBUS, SIG_IGN);
+    }
+
+    siglongjmp(env, 1);
 }
 
-void handle_segfault(__attribute__((unused)) int sig)
-{
-    (void)signal(SIGSEGV, SIG_IGN);
-    siglongjmp(no_env, 1);
-}
-
+/**
+ * @brief Returns how many memory regions there are in the process's address space
+ * and lists them in @p regions
+ * 
+ * The function does so by iterating through every page within the address space,
+ * attempting to push what they are allowed to do. If it fails on first try,
+ * it's memory you have no access to. If it fails afterwards, it's read-only.
+ * If it succeeds all the way through, it's read-writeable.
+ * 
+ * @param regions list of memory regions @see memregion for memory region definition
+ * @param size of list @p regions
+ * @return int count of memory regions ( @see memregion ) in the precess's address space
+ */
 int get_mem_layout(struct memregion *regions, unsigned int size)
 {
+    int jump_val;
+    int check_val; // where we decide what accessibility is for the current pafe
+    unsigned int curr_index = 0u; // current index we're writing on regions
+    unsigned int region_from = 0x0u;
+    int curr_region_mode = MEM_NO; // current region mode
+    bool is_first_loop = true;
 
-    /*
-    We always know that the memory is NO at start
-    */
-    // todo: assert regions exists and its size, i.e., the number ofentries, is equal to size
-    // todo: change numbers
-    // assumption 32 bit
-
-    // assume first address is NO
-    int no_jump_val;
-    int check_val;
-    unsigned int curr_index = 0;
-    unsigned int region_from = 0x0;
-    int curr_region_mode = MEM_NO;
-    int first_loop = 1;
-    for (unsigned long curr_add = region_from; curr_add < 0xffffffff; curr_add += PAGE_SIZE)
+    for (unsigned int curr_add = region_from; curr_add < 0xffffffff; curr_add += PAGE_SIZE)
     {
+        /*
+         * Hacky solution:
+         * 
+         * When unsigned int exceeds, it wraps. We stop once we go back to the start.
+         * I was supposed to use unsigned long, but I encountered a warning
+         * because I was converting unsigned long to char *. I just sticked to my initial
+         * solution.
+         */
         if (curr_add < PAGE_SIZE)
         {
-            if (first_loop)
+            if (is_first_loop)
             {
-                first_loop = 0;
+                is_first_loop = false;
             }
             else
             {
@@ -58,24 +73,35 @@ int get_mem_layout(struct memregion *regions, unsigned int size)
             }
         }
 
-        (void)signal(SIGSEGV, handle_segfault);
-        (void)signal(SIGBUS, handle_segfault);
-        no_jump_val = sigsetjmp(no_env, 1);
+        (void)signal(SIGSEGV, handle_signal);
+        (void)signal(SIGBUS, handle_signal);
+        jump_val = sigsetjmp(env, 1);
         check_val = MEM_NO;
 
-        if (no_jump_val == 0)
+        // try you're best to abuse your privileges on the current page
+        if (jump_val == 0)
         {
             char p1 = ((char *)curr_add)[0];
-            no_jump_val = sigsetjmp(no_env, 1);
+            jump_val = sigsetjmp(env, 1);
             check_val = MEM_RO;
 
-            if (no_jump_val == 0)
+            if (jump_val == 0)
             {
                 ((char *)curr_add)[0] = p1;
                 check_val = MEM_RW;
-            }
-        }
+                // can read-write page
 
+            } // else: read-only page
+
+        } // else: cannot read page
+
+        /*
+         * If the current page has a different memory accessibility as the current
+         * memory region, we start a new memory region. We try to write the memory
+         * region (if we still have space in regions) before we start a new one.
+         * We increment our index, even if it exceeds the size. The index will also
+         * be used for determining the size.
+         */
         if (check_val != curr_region_mode)
         {
             if (curr_index < size)
@@ -86,13 +112,15 @@ int get_mem_layout(struct memregion *regions, unsigned int size)
                 ++curr_index;
             }
 
-            // todo: increase size
-            //    printf("%d: %x-%x %d\n", curr_index, region_from, curr_add-1, check_val);
             region_from = curr_add;
             curr_region_mode = check_val;
         }
     }
 
+    /*
+     * We assume that there's at least one memory region. Our final memory region
+     * is assumed to extend to 0xffffffff.
+     */
     if (curr_index < size)
     {
         regions[curr_index].from = (void *)region_from;
@@ -103,6 +131,13 @@ int get_mem_layout(struct memregion *regions, unsigned int size)
     return curr_index + 1;
 }
 
+/**
+ * @brief Prints a list of memory regions or @see memregion
+ * 
+ * @param regions list of memory regions or @see memregion modified by @see get_mem_layout
+ * @param size size of the list @p regions
+ * @param region_count how many regions were counted by @see get_mem_layout
+ */
 void print_mem_layout(struct memregion *regions, unsigned int size, int region_count)
 {
     if (regions == NULL)
@@ -110,13 +145,14 @@ void print_mem_layout(struct memregion *regions, unsigned int size, int region_c
         return;
     }
 
-    // todo: print memlayout
     for (unsigned int i = 0; i < size && i < (unsigned int)region_count; ++i)
     {
         /* 
-         * Reference:
-         * (1) https://stackoverflow.com/questions/9053658/correct-format-specifier-to-print-pointer-or-address
-         * (2) https://stackoverflow.com/questions/12062916/printing-leading-zeroes-for-hexadecimal-in-c/12062957
+         * References:
+         * (1) Jonathan Leffler. Correct format specifier to print pointer or address? 
+         * stackoverflow.com/questions/9053658/correct-format-specifier-to-print-pointer-or-address
+         * (2) aschepler. Printing leading zeroes for hexadecimal in C.
+         * stackoverflow.com/questions/12062916/printing-leading-zeroes-for-hexadecimal-in-c/12062957
          */
         printf(
             "0x%08" PRIXPTR "-0x%08" PRIXPTR " %s\n", 
@@ -127,6 +163,12 @@ void print_mem_layout(struct memregion *regions, unsigned int size, int region_c
     }
 }
 
+/**
+ * @brief Converts @see memregion#mode to const char*
+ * 
+ * @param mode @see memregion#mode
+ * @return const char* 
+ */
 const char* mode_to_string(unsigned char mode) {
     switch(mode) {
         case MEM_RW:
--- C379 Assignment 1/memlayout.h
+++ C379 Assignment 1/memlayout.h
@@ -11,15 +11,16 @@
 #ifndef MEM_LAYOUT_H_
 #define MEM_LAYOUT_H_
 
-#define MEM_RW 0
-#define MEM_RO 1
-#define MEM_NO 2
+#define MEM_RW 0 /**< @brief memory read / write accessibility */
+#define MEM_RO 1 /**< @brief memory read-only accessibility */
+#define MEM_NO 2 /**< @brief no accessbility */
 
 #include <stdio.h> // for printing
 #include <setjmp.h>
 #include <stdlib.h> // for malloc
 #include <signal.h>
 #include <inttypes.h> // for printing proper looking hex codes
+#include <stdbool.h>
 
 /** 
  * @brief Smallest memory space unit assumed by get_mem_layout() with same accessibility
@@ -29,51 +30,71 @@
 extern const unsigned int PAGE_SIZE;
 
 /**
+ * @brief Stores context during sigsetjumps in @see get_mem_layout
+ */
+sigjmp_buf env;
+
+/**
  * @struct memregion
- * @brief memregion is a contiguous area of memory characterized by the same accessibility
+ * @brief memregion or memory region is a contiguous area of memory 
+ * characterized by the same accessibility
  */
 struct memregion
 {
-    void *from; /**< @brief from is the starting address of the memregion */
-    void *to; /**< @brief to is the last address of the memregion */
+    void *from; /**< @brief from is the starting address of the memory region */
+    void *to; /**< @brief to is the last address of the memory region */
 
     /** 
-     * @brief mode is the type of memory accessibility for the memregion,
+     * @brief mode is the type of memory accessibility for the memory region,
      * which can be MEM_RW, or MEM_RO, or MEM_NO 
      */
     unsigned char mode;
 };
 
 /**
- * @brief handles the segfault exception in get_mem_layout
+ * @brief Returns how many memory regions there are in the process's address space
+ * and lists them in @p regions
  * 
- * @param sig 
- */
-void handle_segfault(int sig);
-
-/**
- * @brief Get the mem layout object
+ * If there are more memory regions than the given @p size, we only fill up
+ * first @p size elements @p regions. Check out @see memregions for more details about
+ * what memory regions are.
  * 
- * @param regions 
- * @param size 
- * @return int 
+ * We assume that @p regions is empty, or the user knows that we did not clear the rest
+ * of @p regions for them.
+ * 
+ * @param regions list of memory regions that the function may modify
+ * @param size size of the list @p regions
+ * @return int count of how many memory regions there are in the process's address space
  */
 int get_mem_layout(struct memregion *regions, unsigned int size);
 
-/* todo: memlayout.h also has a definition for a const unsigned 
-int PAGE_SIZE. Usingpre-processor directives memlayout.h should 
-check if USER_PAGE_SIZE is set and to use it as PAGE_SIZE,and 
-if not defined to set PAGE_SIZE to 4096. */
+/**
+ * @brief Handles the segfault exception in get_mem_layout
+ * 
+ * @param sig signal number being handled by the function
+ */
+void handle_signal(int sig);
 
 /**
- * @brief 
+ * @brief Prints each memregion in a readable manner 
  * 
- * @param regions 
- * @param size 
- * @param region_count 
+ * Each memory region in @p regions is formatted as "0xAAAAAAAA - 0xBBBBBBBB CC"
+ * where AAAAAAAA is the hexadecimal value for the start address in the memory region,
+ * BBBBBBBB is the hexadecimal value for the last address in the memory region,
+ * and CC represents memory accessibility which can either be "NO", "RO", or "RW".
+ * 
+ * @param regions list of @see memregion
+ * @param size size of the list @p regions
+ * @param region_count the actual account of memory region based on @see get_mem_layout
  */
 void print_mem_layout(struct memregion *regions, unsigned int size, int region_count);
 
+/**
+ * @brief converts the @see memregion#mode into const char*
+ * 
+ * @param mode memory accessibility in @see memregion#mode
+ * @return const char* 
+ */
 const char* mode_to_string(unsigned char mode);
 
 #endif /* MEM_LAYOUT_H_ */
