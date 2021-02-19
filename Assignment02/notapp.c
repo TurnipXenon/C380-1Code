f66a8a1a84286fcb211238cbb0eff959f2226ca1
Allan Manuba <allanmanuba@gmail.com>
2021-02-18 17:51:41 -0700

    Improve code

--- Assignment02/notapp.c
+++ Assignment02/notapp.c
@@ -1,5 +1,23 @@
+/**
+ * @file notapp.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-02-18
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+// todo: replace poll with epoll
+
 #include <errno.h>
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/inotify.h>
+#include <unistd.h>
+#include <string.h>
 
 #include "argparser.h"
 
@@ -11,64 +29,120 @@
 #define BUF_LEN (1024 * (EVENT_SIZE + 16))
 // endregion todo DELETE
 
-void do_observer_client(notapp_args arg) {
-    int file_desc = inotify_init();
+static void handle_events(int file_desc, int *watch_desc) {
+    // code based on 'man inotify'
+    char buf[4096]
+               __attribute__ ((aligned(__alignof__(struct inotify_event))));
+    const struct inotify_event *event;
+    ssize_t len;
 
-    if (file_desc < 0) {
+    while(1) {
+        // read events
+        len = read(file_desc, buf, sizeof(buf));
+        if (len == -1 && errno != EAGAIN) {
+            perror("read");
+            // todo: disconnect + clean up
+            return;
+        }
+
+        /* From man notify:
+        If the nonblocking read() found no events to read, then
+        it returns -1 with errno set to EAGAIN. In that case,
+        we exit the loop. */
+        if (len <= 0) {
+            break;
+        }
+
+        for (char *ptr = buf; ptr < buf + len;
+                ptr += EVENT_SIZE + event->len) {
+            
+            event = (const struct inotify_event *) ptr;
+
+            /* Identify event type here */
+            if (event->mask & IN_OPEN)
+                printf("IN_OPEN: ");
+            if (event->mask & IN_CLOSE_NOWRITE)
+                printf("IN_CLOSE_NOWRITE: ");
+            if (event->mask & IN_CLOSE_WRITE)
+                printf("IN_CLOSE_WRITE: ");
+
+            /* Print the name of the watched directory */
+
+            /* Print the name of the file */
+            if (event->len)
+                printf("%s", event->name);
+
+            /* Print type of filesystem object */
+            if (event->mask & IN_ISDIR)
+                printf(" [directory]\n");
+            else
+                printf(" [file]\n");
+        }
+    }
+}
+
+void do_observer_client(notapp_args arg) {
+    // todo attach a sigint handle here
+
+    // code base 'man inotify'
+    char buf;
+    int file_desc, poll_num;
+    int watch_desc;
+    nfds_t nfds = 1;
+    struct pollfd fds[1];
+
+    close(file_desc);
+
+    // Initialize inotify
+    file_desc = inotify_init();
+    if (file_desc == -1) {
         perror("inotify_init");
+        // todo: notify disconnection + clean up
         return;
     }
-    
-    int watch_desc = inotify_add_watch(
+
+    // Add watcher
+    watch_desc = inotify_add_watch(
         file_desc,
         arg.fileordir,
         IN_ALL_EVENTS
     );
 
-    if (watch_desc < 0) {
-        perror("inotify_add_watch");
-        return;
-    }
+    // Inotify input
+    fds[0].fd = file_desc;
+    fds[0].events = POLLIN; // todo may need to change???
 
-    char buf[BUF_LEN];
-    int len, i = 0;
+    // Listen to events
+    while(1) {
+        poll_num = poll(fds, nfds, -1);
+        if (poll_num == -1) {
+            if (errno = EINTR) {
+                // reissue system call
+                continue;
+            }
 
-    len = read(file_desc, buf, BUF_LEN);
-
-    if (len < 0) {
-        if (errno == EINTR) {
-            /* need to reissue system call */
-            printf("Need to reissue system call\n");
-        } else {
-            perror("read");
-        }
-        return;
-    } else if (!len) {
-        printf("BUF_LEN too small?\n");
-        return;
-    }
-
-    while (i < len) {
-        struct inotify_event *event;
-
-        event = (struct inotify_event *) &buf[i];
-
-        printf(
-            "wd=%d mask=%u cookie=%u len=%u\n",
-            event->wd,
-            event->mask,
-            event->cookie,
-            event->len
-        );
-
-        if (event->len) {
-            printf("name=%s\n", event->name);
+            // else
+            perror(poll);
+            // todo: notify disconnection + clean up
+            return;
         }
 
-        i += EVENT_SIZE + event->len;
+        if (poll_num > 0) {
+            if (fds[0].revents & POLLIN) {
+                handle_events(file_desc, watch_desc);
+            }
+        }
+
+        break;
     }
+
+    printf("Event size: %d\n", EVENT_SIZE);
+    printf("Buf len: %d\n", BUF_LEN);
 
     printf("Success!\n");
+
+    // todo: how to clean up after sigint???
+    close(file_desc);
 }
 
 int main(int argc, char *argv[]) {
