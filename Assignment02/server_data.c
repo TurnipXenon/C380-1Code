b5f0ca807e823bae44bb0ea5791e9c4039633a7f
Allan Manuba <allanmanuba@gmail.com>
2021-03-19 04:48:56 -0600

    Document server_data

--- Assignment02/server_data.c
+++ Assignment02/server_data.c
@@ -3,12 +3,16 @@
 static bool possible_lock_detected = false;
 static bool timer_expired = false;
 static pthread_mutex_t timer_expired_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/**
+ * @brief Determines when the state transitions from READING to SORTING
+ */
 void set_timer_expired(bool status) {
     pthread_mutex_lock(&timer_expired_mutex);
 
     if (status && timer_expired) {
-        // if time has expired and previous status was still expired
-        printf("Lock detected >.<\n");
+        /* if time has expired and previous status was still expired 
+           a possible lock may have happened */
         possible_lock_detected = true;
     } else {
         possible_lock_detected = false;
@@ -18,52 +22,85 @@ void set_timer_expired(bool status) {
     pthread_mutex_unlock(&timer_expired_mutex);
 }
 
+/**
+ * @brief Checks if timer has expired
+ * 
+ * @return true 
+ * @return false 
+ */
 bool get_timer_expired() {
     return timer_expired;
 }
 
+/**
+ * @brief Checks if a lock was detected
+ * 
+ * @return true 
+ * @return false 
+ */
 bool is_locked() {
     return possible_lock_detected;
 }
 
 
+
 static enum server_state server_state = DONE;
 static pthread_mutex_t server_state_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/**
+ * @brief Set the server state
+ * 
+ * @param server_state 
+ */
 void set_server_state(enum server_state state) {
     pthread_mutex_lock(&server_state_mutex);
     server_state = state;
     pthread_mutex_unlock(&server_state_mutex);
 }
 
+/**
+ * @brief Get the server state object
+ * 
+ * @return enum server_state 
+ */
 enum server_state get_server_state() {
     return server_state;
 }
 
 
+
 static int user_count = 0;
 static pthread_mutex_t user_count_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/**
+ * @brief Increase connected user client count
+ */
 void increment_user_count() {
     pthread_mutex_lock(&user_count_mutex);
     ++user_count;
     pthread_mutex_unlock(&user_count_mutex);
 }
 
+
+/**
+ * @brief Decrease connected user client count
+ */
 void decrement_user_count() {
     pthread_mutex_lock(&user_count_mutex);
     if (user_count > 0) {
         --user_count;
-    } else {
-        printf("Warning: decrementing user count when empty");
     }
     pthread_mutex_unlock(&user_count_mutex);
 }
 
 
+
 static pthread_mutex_t reader_count_mutex = PTHREAD_MUTEX_INITIALIZER;
 static int reader_done_count = 0;
 static int reader_count = 0;
+
 /**
- * @brief Get the reading user count object
+ * @brief Get how many reading user threads there are
  * 
  * @return int 
  * READERS can't really access until the server state is at reading no need for a mutex
@@ -72,28 +109,42 @@ int get_reading_user_count() {
     return reader_count;
 }
 
-int are_readers_done() {
+/**
+ * @brief Checks how many user threads are done sending all their entries
+ * 
+ * @return true 
+ * @return false 
+ */
+bool are_readers_done() {
     return reader_done_count >= reader_count;
 }
 
+/**
+ * @brief Resets the count of readers done sending for the next round of sending
+ */
 void reset_reader_done_count() {
     reader_done_count = 0;
 }
 
-void increment_reading_user_count();
-void decrement_reading_user_count();
-
+/**
+ * @brief Register a reader thread, equivalent to increasing the reader count
+ */
 void register_reader() {
     pthread_mutex_lock(&reader_count_mutex);
     ++reader_count;
     pthread_mutex_unlock(&reader_count_mutex);
 }
 
+/**
+ * @brief Unregister a reader thread, equivalent to decreasing the reader count
+ */
 void unregister_reader() {
     pthread_mutex_lock(&reader_count_mutex);
     --reader_count;
     pthread_mutex_unlock(&reader_count_mutex);
 }
+
+
 
 static int observer_count = 0;
 static int writer_count = 0;
@@ -103,7 +154,7 @@ static struct user_entry entry_array[CLIENT_MAX];
 static pthread_mutex_t entry_array_register_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 /**
- * @brief Get the entry writer count object
+ * @brief Get how many observer threads there are accessing the entry list
  * 
  * This function will only be used during SORTING state.
  * At that point, all writers will try to finish their jobs and just yield.
@@ -113,16 +164,17 @@ static pthread_mutex_t entry_array_register_mutex = PTHREAD_MUTEX_INITIALIZER;
  * @return int 
  */
 int get_entry_writer_count() {
-    // assert(get_server_state == SORTING);
     return writer_count;
 }
 
+/**
+ * @brief Register an observer_thread, equivalent to increasing the writer count
+ * 
+ * @return int writer index
+ */
 int register_writer() {
     int index = -1;
 
-    // todo: improve???
-    // we need to gain exclusive access to adding a spot in the array
-    // find an empty spot in the array
     pthread_mutex_lock(&entry_array_register_mutex);
 
     ++observer_count;
@@ -142,7 +194,7 @@ int register_writer() {
             index = entry_array_size - 1;
             entry_array[index].is_taken = true;
         } else {
-            // fail
+            /* fail */
             --entry_array_size;
             --observer_count;
         }
@@ -153,6 +205,9 @@ int register_writer() {
     return index;
 }
 
+/**
+ * @brief Register an observer_thread, equivalent to increasing the writer count
+ */
 void unregister_writer(int index) {
     printf("Unregistering: %d\n", index);
     if (index == -1 || index >= CLIENT_MAX) {
@@ -167,9 +222,13 @@ void unregister_writer(int index) {
     --observer_count;
 }
 
-void reset_entry_array() {
-}
-
+/**
+ * @brief Function used for qsort
+ * 
+ * @param elem1 
+ * @param elem2 
+ * @return int 
+ */
 int sort_entry_array_comp(const void *elem1, const void *elem2) {
     struct user_entry *entry1 = ((struct user_entry*)elem1);
     struct user_entry *entry2 = ((struct user_entry*)elem2);
@@ -180,15 +239,21 @@ int sort_entry_array_comp(const void *elem1, const void *elem2) {
     return 0;
 }
 
+/**
+ * @brief Sort entries in entry array
+ * 
+ * Qsort reference from https://www.tutorialspoint.com/c_standard_library/c_function_qsort.htm
+ */
 void sort_entry_array() {
     qsort(entry_array, entry_array_size, sizeof(struct user_entry), sort_entry_array_comp);
-    /* todo: sort array here */
-    // entry_array_size = 0;
 }
 
-/* https://www.tutorialspoint.com/c_standard_library/c_function_qsort.htm */
-void output_entry_sorter();
-
+/**
+ * @brief Adds an entry into the entry array
+ * 
+ * @param struct user_entry
+ * @param int writer index where to overwrite the
+ */
 void add_entry(struct user_entry *entry, int index) {
     while (get_server_state() != DONE) {
         sched_yield();
@@ -206,6 +271,12 @@ void add_entry(struct user_entry *entry, int index) {
     pthread_mutex_unlock(&writer_count_mutex);
 }
 
+/**
+ * @brief Send all entries to a user client by sending how many entries there are followed
+ * by all the entries using send_string(). Receiver are expected to use read_string().
+ * 
+ * @param socket 
+ */
 void send_entries(int socket) {
     // say how many entries
     printf("[Cupcake] Sending observer count: %d\n", observer_count);
@@ -230,14 +301,16 @@ void test_stub() {
     }
 }
 
+/**
+ * @brief Prints the logfile.
+ * 
+ * @param logfile
+ */
 void print_logfile(char *logfile) {
-    // assume not null
-
     FILE *fp;
     fp = fopen(logfile, "a");
 
     if (fp == NULL) {
-        printf("Unable to open %s\n", logfile);
         return;
     }
     
--- Assignment02/server_data.h
+++ Assignment02/server_data.h
@@ -10,6 +10,9 @@
 
 #define CLIENT_MAX 256
 
+/**
+ * @brief Keeps strings to be sent to user clients with metadata for sorting purposes
+ */
 struct user_entry {
     bool is_taken;
     struct timeval tv;
@@ -43,44 +46,138 @@ enum server_state {
 void test_stub();
 
 /**
- * @brief Determines when the state transitions to SORTING
+ * @brief Determines when the state transitions from DONE to SORTING
  * 
- * This is only accessed by server_timer and output_sorter
+ * This should only be used by server_timer and output_sorter
  */
 void set_timer_expired(bool status);
+
+/**
+ * @brief Checks if timer has expired
+ * 
+ * @return true 
+ * @return false 
+ */
 bool get_timer_expired();
 
-enum server_state get_server_state();
+/**
+ * @brief Checks if a lock was detected
+ * 
+ * A lock occurs when output_sorter gets stuck, and timer has already expired again.
+ * 
+ * @return true 
+ * @return false 
+ */
+bool is_locked();
+
+
+
+/**
+ * @brief Set the server state
+ * 
+ * This should only be used by output_sorter
+ * 
+ * @param server_state 
+ */
 void set_server_state(enum server_state server_state);
 
+/**
+ * @brief Get the server state object
+ * 
+ * @return enum server_state 
+ */
+enum server_state get_server_state();
+
+/**
+ * @brief Increase connected user client count
+ */
 void increment_user_count();
+
+/**
+ * @brief Decrease connected user client count
+ */
 void decrement_user_count();
 
-int are_readers_done();
+
+
+/**
+ * @brief Get how many reading user threads there are
+ * 
+ * @return int 
+ * READERS can't really access until the server state is at reading no need for a mutex
+ */
 int get_reading_user_count();
+
+/**
+ * @brief Checks how many user threads are done sending all their entries
+ * 
+ * @return true 
+ * @return false 
+ */
+bool are_readers_done();
+
+/**
+ * @brief Resets the count of readers done sending for the next round of sending
+ */
 void reset_reader_done_count();
-void increment_reading_user_count();
-void decrement_reading_user_count();
+
+/**
+ * @brief Register a reader thread, equivalent to increasing the reader count
+ */
 void register_reader();
+
+/**
+ * @brief Unregister a reader thread, equivalent to decreasing the reader count
+ */
 void unregister_reader();
 
 /**
- * @brief Get the writing state object
+ * @brief Get how many observer threads there are accessing the entry list
  * 
- * @return
+ * @return int writer index used for add_entry()
  */
 int get_entry_writer_count();
+
+/**
+ * @brief Register an observer_thread, equivalent to increasing the writer count
+ * 
+ * @return int 
+ */
 int register_writer();
+
+/**
+ * @brief Register an observer_thread, equivalent to increasing the writer count
+ */
 void unregister_writer(int index);
-void reset_entry_array();
+
+/**
+ * @brief Sort entries in entry array
+ * 
+ * Qsort reference from https://www.tutorialspoint.com/c_standard_library/c_function_qsort.htm
+ */
 void sort_entry_array();
-void send_entries(int socket);
-/* https://www.tutorialspoint.com/c_standard_library/c_function_qsort.htm */
-void output_entry_sorter();
+
+/**
+ * @brief Adds an entry into the entry array
+ * 
+ * @param struct user_entry
+ * @param int writer index where to overwrite the entry
+ */
 void add_entry(struct user_entry *entry, int index);
 
-void print_logfile(char *logfile);
+/**
+ * @brief Send all entries to a user client by sending how many entries there are followed
+ * by all the entries using send_string(). Receiver are expected to use read_string().
+ * 
+ * @param socket 
+ */
+void send_entries(int socket);
 
-bool is_locked();
+/**
+ * @brief Prints the logfile.
+ * 
+ * @param logfile
+ */
+void print_logfile(char *logfile);
 
 #endif /* _SERVER_DATA_H_ */
