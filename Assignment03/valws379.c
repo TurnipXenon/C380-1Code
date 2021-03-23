12c1bbff7b649c77ceaf580ef715104b61cbbc59
Allan Manuba <allanmanuba@gmail.com>
2021-03-23 17:53:11 -0600

    Start assignment 3

---
+++ Assignment03/valws379.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    unsigned int skipsize = 0u;
+
+
+    if (argc == 3 && strcmp(argv[1], "-s") == 0) {
+        skipsize = (unsigned int) atoi(argv[2]);
+        // todo: put precautions of negative value
+        printf("skipsize: %d\n", skipsize);
+
+        // todo: additional powers of two check
+        // todo: check if less than
+    } else if (argc != 0) {
+        printf("todo: Arg error\n");
+        return 0;
+    }
+
+    return 0;
+}
